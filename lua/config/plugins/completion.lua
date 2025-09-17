81b2d22c15c948eccf59717bb0b68f83eeb9e79b
jperparas <j.s.perparas@gmail.com>
2025-09-17 00:01:25 -0700

    fixed bug with completion and snippets

--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -1,7 +1,8 @@
 return { {
   'saghen/blink.cmp',
   dependencies = { 'rafamadriz/friendly-snippets',
-    'L3M0N4D3/LuaSnip' },
+    'L3M0N4D3/LuaSnip'
+  },
 
   version = '1.*',
 
--- lua/config/plugins/luasnip.lua
+++ lua/config/plugins/luasnip.lua
@@ -2,10 +2,10 @@ return { {
   "L3MON4D3/LuaSnip",
 
   -- follow latest release.
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = { 'saghen/blink.cmp',
+    "rafamadriz/friendly-snippets" },
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   config = function()
-    require("luasnip.loaders.from_vscode").lazy_load()
     require("luasnip.loaders.from_vscode").lazy_load({
       vim.fn.stdpath("config") .. "/snippets"
     })
