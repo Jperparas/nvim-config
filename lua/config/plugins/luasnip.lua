f2e57170f76afff8bf1db1b352158ba91d709365
jperparas <j.s.perparas@gmail.com>
2025-09-16 19:45:26 -0700

    added custom snippets for java

--- lua/config/plugins/luasnip.lua
+++ lua/config/plugins/luasnip.lua
@@ -6,5 +6,8 @@ return { {
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   config = function()
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load({
+      vim.fn.stdpath("config") .. "/snippets"
+    })
   end
 } }
